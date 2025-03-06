4d5a0d8664921336c6c6064ab1bd1ad5f1eb600f
QuyenNDth <quyenndth02238@fpt.edu.vn>
2025-03-06 11:55:36 +0700

    quyennd

--- src/main/java/SinhVienService.java
+++ src/main/java/SinhVienService.java
@@ -4,7 +4,7 @@ public class SinhVienService {
             throw new IllegalArgumentException("Thông tin không hợp lệ");
         }
         if (tuoiMoi < 18 || tuoiMoi > 30 || diemMoi < 0.0f || diemMoi > 10.0f || kyMoi < 1 || kyMoi > 8) {
-            throw new IllegalArgumentException("Dữ liệu nhập vào không hợp lệ abccc");
+            throw new IllegalArgumentException("Dữ liệu nhập vào không hợp lệ");
         }
         sv.ten = tenMoi;
         sv.tuoi = tuoiMoi;
